a4772385ea2c96bbc860a6bb91839c47d285cb25
Caspar Gutsche <caspargutsche1@gmail.com>
2023-04-13 18:49:22 +0200

    start new with largrangians

--- Simulation.jl
+++ Simulation.jl
@@ -1,11 +1,4 @@
-using PhysicalConstants
-# define the forces
-function F_ab(ma,mb, r)
-    r_hat = r/norm(r)
-    ma_hat = ma/norm(ma)
-    mb_hat = mb/norm(mb)
-    return 3*μ0 * (ma * mb) /(4π * r^4)*(r_hat .* (ma_hat * mb_hat) + ma .* (r_hat * mb) + mb*(r_hat * ma))- 5 .*r_hat.*(r_hat * ma_hat) * (r_hat * mb_hat) * (ma * mb)
-end
-F_ba() = -F_ab()
+using Zygote
+using Plots
 
-# define the torque
+
